8c3dbcb61b1f626e5db9dfa15f2643058a57f23b
fankunqian <fkq3898885>
2018-01-26 15:34:08 +0800

    update

---
+++ 继承/index.js
@@ -0,0 +1,6 @@
+function _extend(parent,child){
+    var EmptyFn = function(){};
+    EmptyFn.prototype = parent.prototype;
+    child.prototype = new EmptyFn();
+    child.prototype.constructor = child;
+}
